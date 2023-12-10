05a12bcbce894738ca2abe1ba4a778bc8e939d52
lwmoreira <lwmoreira01@gmail.com>
2023-12-10 12:42:24 -0300

    finish-project-supermario

---
+++ script.js
@@ -0,0 +1,17 @@
+let form = document.querySelector('.contact-us')
+let masc = document.querySelector('.masc-form')
+let audioItsmeMario = document.querySelector('.audioItsmeMario')
+let submitAudio = document.querySelector('.submit-audio')
+
+function cliqueiNoBotao() {
+    form.style.left = "400px"
+    masc.style.visibility = "visible"
+    submitAudio.play();
+
+}
+
+function sendForm() {
+    form.style.left = "-258px"
+    masc.style.visibility = "hidden"
+    audioItsmeMario.play();
+ }
